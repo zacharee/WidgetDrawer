b31bb0f84502aaf360e7b576c7d2b69dba7fdacc
zachary <zachary.wander@gmail.com>
2018-11-21 08:15:23 -0500

    Add spring animation

--- app/src/main/java/tk/zwander/widgetdrawer/views/ToolbarAnimHolder.kt
+++ app/src/main/java/tk/zwander/widgetdrawer/views/ToolbarAnimHolder.kt
@@ -1,7 +1,5 @@
 package tk.zwander.widgetdrawer.views
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.animation.TimeInterpolator
 import android.content.Context
 import android.util.AttributeSet
@@ -11,6 +9,9 @@ import android.view.ViewConfiguration
 import android.view.animation.AnticipateInterpolator
 import android.view.animation.OvershootInterpolator
 import android.widget.LinearLayout
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import kotlinx.android.synthetic.main.drawer_layout.view.*
 import tk.zwander.widgetdrawer.R
 import tk.zwander.widgetdrawer.utils.dpAsPx
@@ -18,13 +19,25 @@ import kotlin.math.absoluteValue
 
 class ToolbarAnimHolder : LinearLayout {
     constructor(context: Context) : super(context)
-    constructor(context: Context, attributeSet: AttributeSet) : super (context, attributeSet)
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
 
     private val closedTranslation: Int
         get() = action_bar_wrapper.height
     private val openedTranslation = -context.dpAsPx(16)
-    private val overdragLimit  = openedTranslation * 4
     private val touchListener = TouchListener()
+
+    private val openAnim by lazy {
+        SpringAnimation(this, DynamicAnimation.TRANSLATION_Y, openedTranslation.toFloat()).apply {
+            spring = SpringForce(openedTranslation.toFloat())
+            spring.dampingRatio = SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY
+        }
+    }
+    private val closeAnim by lazy {
+        SpringAnimation(this, DynamicAnimation.TRANSLATION_Y, closedTranslation.toFloat()).apply {
+            spring = SpringForce(closedTranslation.toFloat())
+            spring.dampingRatio = SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY
+        }
+    }
 
     private var wasDragging = false
     private var isOpen = false
@@ -41,28 +54,21 @@ class ToolbarAnimHolder : LinearLayout {
         action_bar_wrapper.setOnTouchListener(touchListener)
     }
 
-    private fun transition(isOpen: Boolean = this.isOpen, duration: Long = Drawer.ANIM_DURATION) {
+    private fun transition(isOpen: Boolean = this.isOpen) {
         if (!currentlyTransitioning) {
             currentlyTransitioning = true
 
-            val newTranslation = if (isOpen) closedTranslation else openedTranslation
+            (if (isOpen) closeAnim else openAnim).apply {
+                addEndListener { _, _, _, _ ->
+                    currentlyTransitioning = false
 
-            animate()
-                .translationY(newTranslation.toFloat())
-                .setDuration(duration)
-                .setInterpolator(if (isOpen) AnticipateInterpolator() as TimeInterpolator else OvershootInterpolator())
-                .setListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator?) {
-                        currentlyTransitioning = false
-
-                        open_close_toolbar.animate()
-                            .scaleY(if (isOpen) 1f else -1f)
-                            .setDuration(Drawer.ANIM_DURATION)
-                            .setInterpolator(if (isOpen) AnticipateInterpolator() as TimeInterpolator else OvershootInterpolator())
-                            .start()
-                    }
-                })
-                .start()
+                    open_close_toolbar.animate()
+                        .scaleY(if (isOpen) 1f else -1f)
+                        .setDuration(Drawer.ANIM_DURATION)
+                        .setInterpolator(if (isOpen) AnticipateInterpolator() as TimeInterpolator else OvershootInterpolator())
+                        .start()
+                }
+            }.start()
 
             this.isOpen = !isOpen
         }
@@ -104,7 +110,7 @@ class ToolbarAnimHolder : LinearLayout {
 
                 MotionEvent.ACTION_UP -> {
                     if (wasDragging && translationY >= openedTranslation) {
-                        transition(translationY > (closedTranslation / 3f), 100)
+                        transition(translationY > (closedTranslation / 3f))
                     } else if (translationY < openedTranslation) {
                         transition(isOpen)
                     } else if (v.id != R.id.action_bar_wrapper) {
