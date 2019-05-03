db77a0043d44c15d3400f3eec0907f5a14d05b8f
zacharee <zachary.wander@gmail.com>
2019-05-03 09:35:25 -0400

    Add API level check to workaround

--- app/src/main/java/tk/zwander/widgetdrawer/App.kt
+++ app/src/main/java/tk/zwander/widgetdrawer/App.kt
@@ -2,6 +2,7 @@ package tk.zwander.widgetdrawer
 
 import android.app.Application
 import android.content.SharedPreferences
+import android.os.Build
 import tk.zwander.widgetdrawer.services.DrawerService
 import tk.zwander.widgetdrawer.utils.PrefsManager
 import java.lang.reflect.Method
@@ -17,18 +18,20 @@ class App : Application(), SharedPreferences.OnSharedPreferenceChangeListener {
 
         prefs.addPrefListener(this@App)
 
-        val forName = Class::class.java.getDeclaredMethod("forName", String::class.java)
-        val getDeclaredMethod = Class::class.java.getDeclaredMethod(
-            "getDeclaredMethod", String::class.java, arrayOf<Class<*>>()::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val forName = Class::class.java.getDeclaredMethod("forName", String::class.java)
+            val getDeclaredMethod = Class::class.java.getDeclaredMethod(
+                "getDeclaredMethod", String::class.java, arrayOf<Class<*>>()::class.java)
 
-        val vmRuntimeClass = forName.invoke(null, "dalvik.system.VMRuntime") as Class<*>
-        val getRuntime = getDeclaredMethod.invoke(vmRuntimeClass, "getRuntime", null) as Method
-        val setHiddenApiExemptions = getDeclaredMethod.invoke(
-            vmRuntimeClass, "setHiddenApiExemptions", arrayOf(arrayOf<String>()::class.java)) as Method
+            val vmRuntimeClass = forName.invoke(null, "dalvik.system.VMRuntime") as Class<*>
+            val getRuntime = getDeclaredMethod.invoke(vmRuntimeClass, "getRuntime", null) as Method
+            val setHiddenApiExemptions = getDeclaredMethod.invoke(
+                vmRuntimeClass, "setHiddenApiExemptions", arrayOf(arrayOf<String>()::class.java)) as Method
 
-        val vmRuntime = getRuntime.invoke(null)
+            val vmRuntime = getRuntime.invoke(null)
 
-        setHiddenApiExemptions.invoke(vmRuntime, arrayOf("L"))
+            setHiddenApiExemptions.invoke(vmRuntime, arrayOf("L"))
+        }
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
