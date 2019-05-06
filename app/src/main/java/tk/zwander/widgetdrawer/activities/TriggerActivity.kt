c41ee1a6e74e09d2744e9f8a6b7c709d6ac769c0
zacharee <zachary.wander@gmail.com>
2019-05-06 10:47:44 -0400

    Add proper shortcut Activity

--- app/src/main/java/tk/zwander/widgetdrawer/activities/TriggerActivity.kt
+++ app/src/main/java/tk/zwander/widgetdrawer/activities/TriggerActivity.kt
@@ -1,7 +1,10 @@
 package tk.zwander.widgetdrawer.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import tk.zwander.widgetdrawer.R
 import tk.zwander.widgetdrawer.services.DrawerService
 
 class TriggerActivity : AppCompatActivity() {
@@ -9,8 +12,25 @@ class TriggerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        DrawerService.openDrawer(this)
+        if (intent.action == Intent.ACTION_CREATE_SHORTCUT) {
+            val shortcutIntent = Intent(this, TriggerActivity::class.java)
 
-        finish()
+            val resultIntent = Intent()
+            resultIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent)
+            resultIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, resources.getString(R.string.open_widget_drawer))
+
+            val iconRes = Intent.ShortcutIconResource.fromContext(
+                this, R.mipmap.ic_launcher
+            )
+
+            intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconRes)
+
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        } else {
+            DrawerService.openDrawer(this)
+
+            finish()
+        }
     }
 }
